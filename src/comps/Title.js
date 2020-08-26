8114e0b05a6b4596e36c7f2792db9eb86644804d
mohit malviya <56771492+mohitmalviya20@users.noreply.github.com>
2020-08-26 22:53:27 +0530

    Update Title.js

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>m-photos</h1>
+      <h2>My Dearest Pictures</h2>
+      <p>“The best thing about a picture is that it never changes, even when the
+        people in it do.”</p>
     </div>
   )
 }
